3417534a21ecf97ccd4a0547d5afd2202b963c16
ColinvD <22950@ma-web.nl>
2019-02-25 12:17:56 +0100

    start with a basic tower

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -6,7 +6,7 @@ public abstract class Tower : MonoBehaviour
 {
     public bool preview;
     public Sprite sprite;
-    public Collider collider;
+    public Collider2D collider;
     public CircleCollider2D range;
     public Vector3 position;
     public List<Upgrade> leftSideUpgrades;
---
+++ Assets/Scripts/Towers/ArrowTower.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowTower : Tower
+{
+    private bool _preview;
+    private Sprite _sprite;
+    private Collider2D _collider;
+    private CircleCollider2D _range;
+    private Vector3 _position;
+    private List<Upgrade> _leftSideUpgrades;
+    private List<Upgrade> _rightSideUpgrades;
+    private List<int> _specialty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        preview = _preview;
+        sprite = _sprite;
+        collider = _collider;
+        range = _range;
+        position = _position;
+        leftSideUpgrades = _leftSideUpgrades;
+        rightSideUpgrades = _rightSideUpgrades;
+        specialty = _specialty;
+    }
+}
